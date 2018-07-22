972a21fff0036d06508aed005397713c76d993c3
antgerasim <antgerasim@gmail.com>
2018-07-23 00:16:23 +0300

    delete old

--- frontend/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-question></app-question>'
-})
-export class AppComponent {
-  title = 'app';
-}
