c6c82d63fc922edef78dec3fc42af1ab5686ac43
antgerasim <antgerasim@gmail.com>
2018-08-19 23:29:29 +0300

    saved PlayQuizComponent

--- frontend/src/app/play-quiz/play-quiz.component.ts
+++ frontend/src/app/play-quiz/play-quiz.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute } from '@angular/router';
 export class PlayQuizComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(private api: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
